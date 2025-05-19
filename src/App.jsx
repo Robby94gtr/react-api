d576b87026df255116eca80a18eb07656b7559a3
Robby94gtr <robertogramazio94@gmail.com>
2025-05-19 23:36:34 +0200

    [UPD] Fetching Data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+const actorEndpoint = 'https://lanciweb.github.io/demo/api/actors/'
+const actressEndpoint = 'https://lanciweb.github.io/demo/api/actresses/'
 
 
 function App() {
+  // state for actors and actresses
+  const [actors, setActors] = useState([])
+  const [actresses, setActresses] = useState([])
+
+  // get actors array
+  const fetchActors = () => {
+    axios
+      .get(actorEndpoint)
+      .then((response) => {
+        setActors(response.data)
+        // console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  // get actresses array
+  const fetchActresses = () => {
+    axios
+      .get(actressEndpoint)
+      .then((response) => {
+        setActresses(response.data)
+        // console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  // fetch actors and actresses on load
+  useEffect(() => {
+    fetchActors()
+    fetchActresses()
+  }, [])
 
   return (
     <>
